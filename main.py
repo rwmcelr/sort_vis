2404bbfad1bfe36c7d5e2c1deb6ac8c16a02af85
Robert McElroy <75845442+rwmcelr@users.noreply.github.com>
2025-09-18 22:23:04 -0500

    fixed animation, removed unnecessary complexity

--- main.py
+++ main.py
@@ -6,13 +6,11 @@ import matplotlib.animation as animation
 colors = {
     "base": "#dddddd",
     "edge": "#cccccc",
-    "focus": "#E6E6FA",   # lavender
-    "compare": "#FFD8A8", # pastel orange
-    "sorted": "#C8F7D4",  # green
+    "highlight": "#E6E6FA",   # lavender
+    "sorted": "#FFC5D3", # pastel pink
 }
 
 arr = np.random.randint(0, 1000, 25)
-swap_frames = 3
 
 fig, ax = plt.subplots(figsize = (12,4))
 bars = ax.bar(np.arange(len(arr)), arr, width=0.9, align="edge",
@@ -24,27 +22,27 @@ ax.set_xlim(0, len(arr))
 ax.margins(0,0)
 
 def update(frame):
-    positions, bar_order, focus, compare, sorted_start = frame
+    sorting_array, comparison, sorted_left_edge = frame
 
-    for bar, x in zip(bars, positions):
-        bar.set_x(x)
-        bar.set_facecolor(colors['base'])
-        bar.set_edgecolor("#cccccc")
+    for i, bar in enumerate(bars):
+        bar.set_height(sorting_array[i])
+        if i < sorted_left_edge:
+            bar.set_edgecolor(colors["edge"])
+            bar.set_facecolor(colors["base"])
+        else:
+            bar.set_facecolor(colors["sorted"])
 
-    if sorted_start < len(bars):
-        for k in range(sorted_start, len(bars)):
-            original_bar_idx = bar_order[k]
-            bars[original_bar_idx].set_facecolor(colors['sorted'])
-
-    if focus is not None and compare is not None:
-        bars[focus].set_facecolor(colors['focus'])
-        bars[compare].set_facecolor(colors['compare'])
+    if comparison:
+        bars[comparison].set_facecolor(colors['highlight'])
+        if comparison + 1 < sorted_left_edge:
+            bars[comparison+1].set_facecolor(colors['highlight'])
+        if bars[comparison-1]: bars[comparison-1].set_facecolor(colors['highlight'])
 
     return bars
 
 ani = animation.FuncAnimation(
-    fig, update, frames=bubble_sort(arr, swap_frames),
-    interval=50, repeat=False, blit=False, cache_frame_data=False
+    fig, update, frames=bubble_sort(arr),
+    interval=15, repeat=False, cache_frame_data=False
 )
 
 plt.show()
